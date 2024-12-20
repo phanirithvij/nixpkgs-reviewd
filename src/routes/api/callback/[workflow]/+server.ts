c5960c2973ce2a467eefcbb044cc77d1fd079293
lucasew <lucas59356@gmail.com>
2024-12-20 18:33:28 -0300

    add link to pr
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes/api/callback/[workflow]/+server.ts
+++ src/routes/api/callback/[workflow]/+server.ts
@@ -13,6 +13,11 @@ export async function POST(event) {
     return await json({error: "Workflow not found"}, {status: 400})
   }
   const { id, name, status, conclusion, html_url } = selectedWorkflow
+  const prData = name.match('#([0-9]*)')
+  if (!prData) {
+    return await json({error: "Can't detect PR number"}, {status: 400})
+  }
+  const pr = prData[1]
   const triggerData = name.match('for (.*)')
   if (!triggerData) {
     return await json({error: "Can't detect trigger owner"}, {status: 400})
@@ -25,5 +30,5 @@ export async function POST(event) {
   }
   const emoji = workflowEmoji(selectedWorkflow)
 
-  return await json(await methods.telegramReply(`${emoji} **${name}**\n [Result](${html_url})`, triggerChatID), {status: 200})
+  return await json(await methods.telegramReply(`${emoji} **${name}**\n\n• [Result](${html_url})\n• [PR](https://github.com/NixOS/nixpkgs/pull/${pr})`, triggerChatID), {status: 200})
 }
